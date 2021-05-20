12868eb49a8a44400d5fbe365885ccf0d737f7e7
Vamsi Krishna Allamudi <allamudivamsi@gmail.com>
2021-05-20 19:37:37 +0530

    aded progessbar

--- Webpages/class10/ProgressBar/scripts.js
+++ Webpages/class10/ProgressBar/scripts.js
@@ -5,7 +5,10 @@ function barcal(){
     var scrolled = (winScroll / height) * 100;
     console.log(scrolled)
     bar.innerHTML=scrolled.toFixed(0)+"%"
-    bar.style.width=scrolled+"%"    
+    bar.style.width=scrolled+"%"
+
+    bar.style.backgroundColor="rgb(100,"+scrolled/100*255+",100)"
+
 }
 document.addEventListener('scroll',barcal)
 
